e7be1558083c93cf5329094e9f0e6d7e951972d3
Ernie Casilla <ecasilla@icloud.com>
2015-07-27 02:31:10 -0400

    added error middle ware

--- invision_consumer/index.js
+++ invision_consumer/index.js
@@ -47,9 +47,11 @@ app.use(bodyParser.urlencoded({
  * description The catch all request handler for the consumer
  * It will queue all querys for the consumer and send the response once finished 
  */
-app.use(function(req, res){
+app.use(function(req, res,next){
   if (!req.params.data) {
-    res.status(500).end(new Error('The was no data query can\'t process'));
+    var err = new Error('The was no data query can\'t process')
+    err.status = 500;
+    next(err);
   }
   debug('dev')('new request incoming: ' + JSON.stringify(req.params));
   var data = req.params;
@@ -57,14 +59,35 @@ app.use(function(req, res){
   Consumer.enqueue(data,function(err,results){
     if (err) {
       debug('Houston we have a problem',err);
-      res.status(500).end(err);
+      err.status = 500;
+      next(err);
     }else{
-      res.status(200).end(results);
+      res.end(results);
       debug('dev')('task result',results);
       debug('response')('response outgoing');
     }
   });
 });
+
+function errorHandler(){
+  return function errorHandler(err, req, res, next){
+    if (err.status) res.statusCode = err.status;
+    if (res.statusCode < 400) res.statusCode = 500;
+    var accept = req.headers.accept || '';
+    if (~accept.indexOf('json')) {
+      var error = { message: err.message, stack: err.stack };
+      for (var prop in err) error[prop] = err[prop];
+      var json = JSON.stringify({ error: error });
+      res.setHeader('Content-Type', 'application/json');
+      res.end(json);
+    } else {
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(err.stack);
+    }
+  };
+};
+
+app.use(errorHandler());
 
 http.createServer(app).listen(port,function(){
  debug('dev')('listening on port %d',port);
